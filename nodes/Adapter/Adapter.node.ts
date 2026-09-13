f95f8a9e95f55368de373c2cdde9dd021a4ceda7
incluc0de <incluc0de.ai@gmail.com>
2026-09-13 18:38:09 -0300

    node Adapter

---
+++ nodes/Adapter/Adapter.node.ts
@@ -0,0 +1,78 @@
+import type {
+	IExecuteFunctions,
+	INodeExecutionData,
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+import { NodeConnectionTypes, NodeOperationError } from 'n8n-workflow';
+
+export class Adapter implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Incluc0de Adapter',
+		name: 'Adapter',
+		icon: { light: 'file:adapter.svg', dark: 'file:adapter.dark.svg' },
+		group: ['input'],
+		version: [1],
+		description: 'Incluc0de Adapter Node',
+		defaults: {
+			name: 'Adapter',
+		},
+		inputs: [NodeConnectionTypes.Main],
+		outputs: [NodeConnectionTypes.Main],
+		usableAsTool: false,
+		properties: [
+			// Node properties which the user gets displayed and
+			// can change on the node.
+			{
+				displayName: 'Incluc0de Adapter',
+				name: 'Adapter',
+				type: 'string',
+				default: '',
+				placeholder: 'Incluc0de Adapter',
+				description: 'Incluc0de Adapter Node',
+			},
+		],
+	};
+
+	// The function below is responsible for actually doing whatever this node
+	// is supposed to do. In this case, we're just appending the `myString` property
+	// with whatever the user has entered.
+	// You can make async calls and use `await`.
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		let item: INodeExecutionData;
+		let myString: string;
+
+		// Iterates over all input items and add the key "myString" with the
+		// value the parameter "myString" resolves to.
+		// (This could be a different value for each item in case it contains an expression)
+		for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+			try {
+				myString = this.getNodeParameter('myString', itemIndex, '') as string;
+				item = items[itemIndex];
+
+				item.json.myString = myString;
+			} catch (error) {
+				// This node should never fail but we want to showcase how
+				// to handle errors.
+				if (this.continueOnFail()) {
+					items.push({ json: this.getInputData(itemIndex)[0].json, error, pairedItem: itemIndex });
+				} else {
+					// Adding `itemIndex` allows other workflows to handle this error
+					if (error.context) {
+						// If the error thrown already contains the context property,
+						// only append the itemIndex
+						error.context.itemIndex = itemIndex;
+						throw error;
+					}
+					throw new NodeOperationError(this.getNode(), error, {
+						itemIndex,
+					});
+				}
+			}
+		}
+
+		return [items];
+	}
+}
